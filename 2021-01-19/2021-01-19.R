bfbb3626e2eab9b33f62f3aaa6517cd05608daff
Jenn Schilling <jathorne2@gmail.com>
2021-01-19 18:26:49 -0700

    initial commit for 4

---
+++ 2021-01-19/2021-01-19.R
@@ -0,0 +1,13 @@
+# Author : Jenn Schilling
+# Title: #TidyTuesday Kenya Census
+# Date: Jan 19 2021
+
+#### Libraries ####
+
+library(tidyverse)
+
+#### Get the Data ####
+
+gender <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-01-19/gender.csv')
+crops <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-01-19/crops.csv')
+households <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-01-19/households.csv')
