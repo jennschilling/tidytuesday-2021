431b62c2e2152c550c1a30869e6cd455268255db
Jenn Schilling <jathorne2@gmail.com>
2021-03-02 17:06:25 -0700

    week 10 - initial commit

---
+++ 2021-03-02/2021-03-02.R
@@ -0,0 +1,17 @@
+# Author : Jenn Schilling
+# Title: #TidyTuesday Superbowl Commercials
+# Date: Mar 2 2021
+
+#### Libraries ####
+
+library(tidyverse)
+library(extrafont)
+
+#### Data #### 
+
+youtube <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-03-02/youtube.csv')
+
+
+#### Formatting ####
+
+font <- ""
