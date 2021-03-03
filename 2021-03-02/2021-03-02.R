e4e3a7ef6c4f85d492a1324ac9f86955050d6d3b
jaschilling <jaschilling@arizona.edu>
2021-03-03 11:25:08 -0700

    week 10 - finished formatting and saved plots

--- 2021-03-02/2021-03-02.R
+++ 2021-03-02/2021-03-02.R
@@ -27,19 +27,19 @@ theme_set(theme_minimal(base_size = 12, base_family = font))
 
 theme_update(
   panel.grid.minor = element_blank(),
-  axis.title = element_text(color = fontcolor),
-  axis.text = element_text(color = fontcolor),
+  axis.title = element_text(size = 9, color = fontcolor),
+  axis.text = element_text(size = 8, color = fontcolor),
   
-  legend.title = element_text(color = fontcolor),
-  legend.text = element_text(color = fontcolor),
+  legend.title = element_text(size = 9, color = fontcolor),
+  legend.text = element_text(size = 8, color = fontcolor),
   
   plot.title.position = "plot",
   plot.title = element_text(size = 16, face = "bold"),
   
-  plot.subtitle = element_text(size = 12, color = fontcolor),
+  plot.subtitle = element_text(size = 9, color = fontcolor, margin = margin(b = 15)),
   
   plot.caption.position = "plot",
-  plot.caption = element_text(size = 10, color = fontcolor),
+  plot.caption = element_text(size = 8, color = fontcolor),
   
   plot.margin = margin(t = 25, r = 25, b = 10, l = 25)
 )
@@ -52,28 +52,48 @@ ggplot(data = youtube,
                      size = comment_count)) +
   geom_point(alpha = 0.7) +
   facet_wrap(~ brand,
-             ncol = 1) +
-  scale_x_continuous(labels = scales::label_number_si()) +
-  scale_y_continuous(labels = scales::label_number_si()) +
+             ncol = 2,
+             strip.position = "left",
+             scales = "free") +
+  scale_x_continuous(limits = c(0, 180000000),
+                     breaks = scales::pretty_breaks(),
+                     labels = scales::label_number_si()) +
+  scale_y_continuous(limits = c(0, 300000),
+                     labels = scales::label_number_si()) +
   scale_size_continuous(labels = scales::comma) +
   coord_cartesian(expand = FALSE, 
                   clip = "off") +
   guides(size = guide_legend(title.position = "top",
                              title.hjust = 0.5)) +
   labs(title = "Superbowl Ads 2000-2020",
-       subtitle = "",
-       x = "Number of Views",
-       y = "Number of Likes",
+       subtitle = "Number of Views vs. Number of Likes",
+       x = "",
+       y = "",
        size = "Number of Comments",
        caption = "Source: FiveThirtyEight | Viz: Jenn Schilling") +
-  theme(plot.title.position = "plot",
-        plot.caption.position = "plot",
+  theme(plot.subtitle = element_markdown(size = 10),
         
-        strip.text = element_text(size = 12,
-                                  hjust = 0),
+        plot.caption = element_markdown(),
+        
+        strip.text.y.left = element_text(size = 10,
+                                         angle = 0,
+                                         vjust = 1,
+                                         hjust = 1,
+                                         face = "bold"),
+        
+        strip.placement = "outside",
+        
+        panel.spacing.y = unit(0.5, "cm"),
+        panel.spacing.x = unit(0.5, "cm"),
         
         legend.position = "bottom")
 
+ggsave("2021-03-02\\superbowl_1.png",
+       plot = last_plot(),
+       device = "png",
+       width = 8,
+       height = 7,
+       type = "cairo")
 
 # Remove outlier of Doritos 2017 Sling Baby
 
@@ -81,28 +101,50 @@ ggplot(data = youtube %>% filter(view_count < 170000000),
        mapping = aes(x = view_count, 
                      y = like_count,
                      size = comment_count)) +
-  geom_point(color = "gray30") +
+  geom_point(alpha = 0.7) +
   facet_wrap(~ brand,
-             ncol = 1) +
-  scale_x_continuous(labels = scales::label_number_si()) +
-  scale_y_continuous(labels = scales::label_number_si()) +
+             ncol = 2,
+             strip.position = "left",
+             scales = "free") +
+  scale_x_continuous(limits = c(0, 29000000),
+                     breaks = scales::pretty_breaks(),
+                     labels = scales::label_number_si()) +
+  scale_y_continuous(limits = c(0, 180000),
+                     labels = scales::label_number_si()) +
   scale_size_continuous(labels = scales::comma) +
   coord_cartesian(expand = FALSE, 
                   clip = "off") +
   guides(size = guide_legend(title.position = "top",
                              title.hjust = 0.5)) +
   labs(title = "Superbowl Ads 2000-2020",
-       subtitle = "Doritos 2017 *Sling Baby* ad, which had over 170M views, excluded",
-       x = "Number of Views",
-       y = "Number of Likes",
+       subtitle = "Number of Views vs. Number of Likes",
+       x = "",
+       y = "",
        size = "Number of Comments",
-       caption = "Source: FiveThirtyEight | Viz: Jenn Schilling") +
+       caption = "Source: FiveThirtyEight | Viz: Jenn Schilling <br><br>
+       Doritos' 2017 *Sling Baby* ad, which had over 170M views, has been excluded") +
   theme(plot.subtitle = element_markdown(),
         
-        strip.text = element_text(size = 12,
-                                  hjust = 0),
+        plot.caption = element_markdown(),
+        
+        strip.text.y.left = element_text(size = 10,
+                                         angle = 0,
+                                         vjust = 1,
+                                         hjust = 1,
+                                         face = "bold"),
+        
+        strip.placement = "outside",
+        
+        panel.spacing.y = unit(0.5, "cm"),
+        panel.spacing.x = unit(0.5, "cm"),
         
         legend.position = "bottom")
 
+ggsave("2021-03-02\\superbowl_2.png",
+       plot = last_plot(),
+       device = "png",
+       width = 8,
+       height = 7,
+       type = "cairo")
 
 
