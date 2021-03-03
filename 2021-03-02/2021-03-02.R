3b8c1974e7b3f9b229b0ff4aae49a84c11092156
jaschilling <jaschilling@arizona.edu>
2021-03-03 09:12:53 -0700

    week 11 - added plot without outlier of views

--- 2021-03-02/2021-03-02.R
+++ 2021-03-02/2021-03-02.R
@@ -7,6 +7,7 @@
 library(tidyverse)
 library(extrafont)
 library(ggrepel)
+library(ggtext)
 
 #### Data #### 
 
@@ -23,7 +24,7 @@ font <- "Gill Sans MT"
 
 
 
-#### Data Exploration ####
+#### Plot ####
 
 ggplot(data = youtube,
        mapping = aes(x = view_count, 
@@ -36,7 +37,7 @@ ggplot(data = youtube,
   scale_y_continuous(labels = scales::label_number_si()) +
   scale_size_continuous(labels = scales::comma) +
   labs(title = "Superbowl Ads 2000-2020",
-       subtitle = "Greater number of views tends to correspond with greater number of likes and comments.",
+       subtitle = "",
        x = "Number of Views",
        y = "Number of Likes",
        size = "Number of Comments",
@@ -51,5 +52,38 @@ ggplot(data = youtube,
                                   hjust = 0),
         
         legend.position = "bottom")
+
+
+# Remove outlier of Doritos 2017 Sling Baby
+
+ggplot(data = youtube %>% filter(view_count < 170000000),
+       mapping = aes(x = view_count, 
+                     y = like_count,
+                     size = comment_count)) +
+  geom_point(color = "gray30") +
+  facet_wrap(~ brand,
+             ncol = 1) +
+  scale_x_continuous(labels = scales::label_number_si()) +
+  scale_y_continuous(labels = scales::label_number_si()) +
+  scale_size_continuous(labels = scales::comma) +
+  labs(title = "Superbowl Ads 2000-2020",
+       subtitle = "Doritos 2017 *Sling Baby* ad, which had over 170M views, excluded",
+       x = "Number of Views",
+       y = "Number of Likes",
+       size = "Number of Comments",
+       caption = "Source: FiveThirtyEight | Viz: Jenn Schilling") +
+  theme_minimal() +
+  theme(text = element_text(family = font),
+        
+        plot.subtitle = element_markdown(),
+        
+        plot.title.position = "plot",
+        plot.caption.position = "plot",
+        
+        strip.text = element_text(size = 12,
+                                  hjust = 0),
+        
+        legend.position = "bottom")
+
 
 
