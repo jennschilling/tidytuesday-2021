0f39db8f6a3938a4c4930edd5b1c285b4cfdf146
jaschilling <jaschilling@arizona.edu>
2021-03-25 17:17:16 -0700

    week 13 - created combined plot and function to make plots

--- 2021-03-23/2021-03-23.R
+++ 2021-03-23/2021-03-23.R
@@ -6,6 +6,7 @@
 
 library(tidyverse)
 library(extrafont)
+library(patchwork)
 
 #### Data #### 
 
@@ -42,7 +43,8 @@ n_votes <-  un_combined %>%
   group_by(year, issue) %>%
   summarise(n = n(),
             .groups = 'drop') %>%
-  ungroup()
+  ungroup() %>%
+  mutate(issue = factor(ifelse(is.na(issue), "Unknown", as.character(issue))))
 
 #### Formatting ####
 
@@ -53,41 +55,79 @@ theme_set(theme_minimal(base_size = 12, base_family = font))
 
 theme_update(
   panel.grid.minor = element_blank(),
+  
+  axis.title = element_text(size = 10, color = fontcolor),
   axis.text = element_text(size = 9, color = fontcolor),
   
-  legend.title = element_text(size = 12, color = fontcolor),
-  legend.text = element_text(size = 10, color = fontcolor),
-  
   plot.title.position = "plot",
-  plot.title = element_text(size = 16, color = fontcolor),
+  plot.title = element_text(size = 12, color = fontcolor),
   
-  plot.subtitle = element_text(size = 12, color = fontcolor),
+  plot.subtitle = element_text(size = 10, color = fontcolor),
   
   plot.caption.position = "plot",
   plot.caption = element_text(size = 7, color = fontcolor),
   
-  plot.margin = margin(t = 25, r = 25, b = 10, l = 25)
+  plot.margin = margin(t = 5, r = 10, b = 5, l = 10)
 )
 
 #### Make Plot ####
 
-ggplot(data = subset(un_combined_summary,
-                     original == 1 & vote == 'yes' &
-                       country != 'United States' &
-                       issue != 'Unknown'),
-       mapping = aes(x = year,
-                     y = pct)) +
-  geom_point(color = 'gray50', 
-             alpha = 0.25) +
-  geom_point(data = subset(un_combined_summary,
-                           original == 1 & vote == 'yes' &
-                             country == 'United States' &
-                             issue != 'Unknown'),
-             mapping = aes(x = year,
-                           y = pct),
-             color = '#1b9e77') +
-  scale_y_continuous(labels = scales::percent) +
-  facet_wrap(~issue)
+# List of issues
+issues <- n_votes %>% 
+  select(issue) %>% 
+  filter(issue != 'Unknown') %>%
+  unique(.) %>%
+  mutate(issue = as.character(issue)) %>%
+  pull(.)
+
+# Function Create individual Plots 
+plot_issues <- function(issue_selected){
+  
+  p1 <- ggplot(data = subset(un_combined_summary,
+                             original == 1 & vote == 'yes' &
+                               country != 'United States' &
+                               issue == issue_selected),
+               mapping = aes(x = year,
+                             y = pct)) +
+    geom_point(color = 'gray50', 
+               alpha = 0.25,
+               size = 1) +
+    geom_point(data = subset(un_combined_summary,
+                             original == 1 & vote == 'yes' &
+                               country == 'United States' &
+                               issue != 'Unknown'),
+               mapping = aes(x = year,
+                             y = pct),
+               color = '#1b9e77',
+               size = 2) +
+    scale_y_continuous(labels = scales::percent) +
+    labs(x = "",
+         y = "",
+         title = issue_selected) 
+  
+  p2 <- ggplot(data = subset(n_votes, issue == issue_selected),
+               mapping = aes(x = year,
+                             y = n)) +
+    geom_line(color = fontcolor) +
+    scale_y_continuous(limits = c(0, 45)) +
+    labs(x = "",
+         y = "") +
+    theme(panel.grid = element_blank(),
+          axis.text = element_blank()) 
+  
+  p1 / p2 + plot_layout(nrow = 2, heights = c(4, 1))
+  
+  
+}
+
+plots <- lapply(issues, plot_issues)
 
 
-ggplot(data = )
+# Create combined plot
+
+(plots[[1]] | plots [[2]] | plots[[3]]) /
+  (plots[[4]] | plots [[5]] | plots[[6]]) +
+  plot_annotation(title = "TEST",
+                  subtitle = "TEST",
+                  caption = "TEST")
+
