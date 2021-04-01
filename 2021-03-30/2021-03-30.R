110ef5c91dcba37c5122e9323d8d003f24256ed3
Jenn Schilling <jathorne2@gmail.com>
2021-03-31 18:54:24 -0700

    week 14 - started some plots

--- 2021-03-30/2021-03-30.R
+++ 2021-03-30/2021-03-30.R
@@ -40,4 +40,48 @@ theme_update(
 
 #### Plot ####
 
+all_shades <- allShades %>%
+  mutate(lightness_group = cut_interval(lightness, 20)) %>%
+  group_by(lightness_group) %>%
+  arrange(sat, .by_group = TRUE) %>%
+  mutate(y = row_number()) %>%
+  ungroup()
+
+ggplot(data = all_shades,
+       mapping = aes(x = lightness_group,
+                     y = y,
+                     fill = hex)) +
+  geom_tile() +
+  scale_fill_identity() +
+  guides(fill = FALSE) +
+  labs(title = "Distribution of all foundation shades from Ulta and Sephora",
+       x = "",
+       y = "") +
+  coord_cartesian(expand = FALSE) +
+  theme(axis.text = element_blank(),
+        panel.grid = element_blank())
+
+
+nude_shades <- allShades %>%
+  filter(name %in% c("Nude", "Neutral")) %>%
+  mutate(lightness_group = cut_interval(lightness, 20)) %>%
+  group_by(lightness_group) %>%
+  arrange(sat, .by_group = TRUE) %>%
+  mutate(y = row_number()) %>%
+  ungroup()
+
+ggplot(data = nude_shades,
+       mapping = aes(x = lightness_group,
+                     y = y,
+                     fill = hex)) +
+  geom_tile(color = 'white') +
+  scale_fill_identity() +
+  guides(fill = FALSE) +
+  labs(title = "Distribution of **Nude/Neutral** foundation shades from Ulta and Sephora",
+       x = "",
+       y = "") +
+  coord_cartesian(expand = FALSE) +
+  theme(axis.text = element_blank(),
+        panel.grid = element_blank())
+
 
