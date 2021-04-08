e8ed2fe96e31ca6722766ad1e480103f5eb6b502
jaschilling <jaschilling@arizona.edu>
2021-04-08 15:23:04 -0700

    made wide version of all plots together

--- 2021-all-plots/combine-plots.R
+++ 2021-all-plots/combine-plots.R
@@ -80,4 +80,54 @@ final <- image_append(c(final_20,
                       stack = TRUE)
 
 image_write(final,
-            "2021-all-plots\\summary.png")
+            "2021-all-plots\\summary_long.png")
+
+
+#### Alternate Layout 4 rows by 7 columns ####
+
+row_1 <- image_append(
+  c(image_border(image_resize(image_read("https://github.com/jennschilling/tidytusesday/raw/master/2020-07-28/tidytuesday-palmerpenguins.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("https://github.com/jennschilling/tidytusesday/raw/master/2020-08-04/top10_european_energy.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("https://github.com/jennschilling/tidytusesday/raw/master/2020-08-11/avatar.ratings.characters.png"), "500x400!"), color = "white", geometry = "10x10"), 
+    image_border(image_resize(image_read("https://github.com/jennschilling/tidytusesday/raw/master/2020-08-18/extinct.plants.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("https://github.com/jennschilling/tidytusesday/raw/master/2020-08-25/chopped.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("https://github.com/jennschilling/tidytusesday/raw/master/2020-09-01/banana.yield.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("https://github.com/jennschilling/tidytusesday/raw/master/2020-09-08/friends_seasons.gif"), "500x400!")[1], color = "white", geometry = "10x10")))
+
+row_2 <- image_append(    
+  c(image_border(image_resize(image_read("https://github.com/jennschilling/tidytusesday/raw/master/2020-09-15/k12_spend_per_child.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("https://github.com/jennschilling/tidytusesday/raw/master/2020-09-22/first_ascents.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("https://github.com/jennschilling/tidytusesday/raw/master/2020-09-29/worldwide_sales.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("https://github.com/jennschilling/tidytusesday/raw/master/2020-11-03/ikea_category_prices.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("https://github.com/jennschilling/tidytusesday/raw/master/2020-11-24/washington_hikes.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("2021-01-12\\turner_oil_colors_circle.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("2021-01-19\\county_female_map.png"), "500x400!"), color = "white", geometry = "10x10")))
+
+
+row_3 <- image_append(
+  c(image_border(image_resize(image_read("2021-01-19\\county_female_bar.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("2021-01-26\\sankey_plastic.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("2021-02-02\\bach_hs_attain_women.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("2021-02-02\\bach_hs_attain.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("2021-02-09\\wealth_distribution.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("2021-02-16\\c1_final.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("2021-02-16\\c2_final.png"), "500x400!"), color = "white", geometry = "10x10")))
+
+
+row_4 <- image_append(
+  c(image_border(image_resize(image_read("2021-02-23\\earnings_age_2020.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("2021-02-23\\occupations_2020.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("2021-03-02\\superbowl_2.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("2021-03-09\\genre_summary.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("2021-03-16\\steam_games.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("2021-03-23\\un_votes.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("2021-03-30\\foundation_shades.png"), "500x400!"), color = "white", geometry = "10x10")))
+
+final_together <- image_append(c(row_1,
+                                 row_2,
+                                 row_3,
+                                 row_4),
+                               stack = TRUE)
+
+image_write(final_together,
+            "2021-all-plots\\summary_wide.png")
