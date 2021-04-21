627613b9d90efb0377203150418fa494231788b1
Jenn Schilling <jathorne2@gmail.com>
2021-04-20 17:58:01 -0700

    week 17 - initial commit

---
+++ 2021-04-20/2021-04-20.R
@@ -0,0 +1,39 @@
+# Author : Jenn Schilling
+# Title: #TidyTuesday Netflix
+# Date: Apr 20 2021
+
+#### Libraries ####
+
+library(tidyverse)
+library(extrafont)
+library(ggtext)
+
+#### Data #### 
+
+netflix_titles <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-04-20/netflix_titles.csv')
+
+#### Formatting ####
+
+font <- "Gill Sans MT"
+fontcolor <- "gray30"
+
+theme_set(theme_minimal(base_size = 12, base_family = font))
+
+theme_update(
+  panel.grid.minor = element_blank(),
+  
+  axis.title = element_text(size = 10, color = fontcolor),
+  axis.text = element_text(size = 9, color = fontcolor),
+  
+  strip.text = element_text(size = 10, color = fontcolor, hjust = 0),
+  
+  plot.title.position = "plot",
+  plot.title = element_markdown(size = 12, color = fontcolor),
+  
+  plot.subtitle = element_markdown(size = 10, color = fontcolor),
+  
+  plot.caption.position = "plot",
+  plot.caption = element_markdown(size = 8, color = fontcolor),
+  
+  plot.margin = margin(t = 15, r = 15, b = 15, l = 15)
+)
