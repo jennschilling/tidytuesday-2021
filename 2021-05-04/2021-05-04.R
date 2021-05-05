f5c1a41c71b47e091faef20620aa0d9377e4d770
jaschilling <jaschilling@arizona.edu>
2021-05-05 14:52:31 -0700

    week 19 - playing with plots

--- 2021-05-04/2021-05-04.R
+++ 2021-05-04/2021-05-04.R
@@ -7,25 +7,28 @@
 library(tidyverse)
 library(extrafont)
 library(ggtext)
+library(ggalluvial)
 
 #### Data #### 
 
 water <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-05-04/water.csv')
 
-uganda <- water %>%
-  filter(country_name == "Uganda") %>%
-  mutate(report_year = parse_number(str_sub(report_date, start = 7, end = 10)),
-         bin_install_year = cut(install_year, breaks = 5, dig.lab = 4),
-         bin_report_year = cut(report_year, breaks = 5, dig.lab = 4))
-
-# Latitude and Longitude data does not seem reliable - need to figure out a different viz
-
-uganda_map <- map_data(map = "world") %>%
-  filter(region == "Uganda")
-
-uganda_clean <- uganda %>%
-  filter(lat_deg <= max(uganda_map$lat) & lat_deg >= min(uganda_map$lat) &
-           lon_deg <= max(uganda_map$long) & lon_deg >= min(uganda_map$long))
+water_alluvial <- water %>% 
+  filter(install_year <= 2021) %>%
+  filter(!is.na(water_source)) %>%
+  group_by(install_year, water_source) %>%
+  summarise(total = n(),
+            .groups = "drop") %>%
+  group_by(install_year) %>%
+  mutate(pct = total / sum(total)) %>%
+  select(-total) %>%
+  pivot_wider(names_from = "water_source",
+              values_from = "pct") %>%
+  pivot_longer(-install_year,
+               names_to = "water_source",
+               values_to = "pct",
+               values_drop_na = FALSE) %>%
+  mutate(pct = ifelse(is.na(pct), 0 , pct))
 
 #### Formatting ####
 
@@ -50,26 +53,21 @@ theme_update(
 
 #### Plot ####
 
-ggplot(data = uganda %>% filter(!is.na(install_year)),
-       mapping = aes(x = install_year,
-                     y = report_year,
-                     color = water_source)) +
-  geom_jitter() +
-  facet_wrap(~ water_source) +
-  guides(color = FALSE)
+ggplot(data = water %>% filter(install_year <= 2021),
+       mapping = aes(y = install_year,
+                     fill = water_source)) +
+  geom_bar()
 
-ggplot() +
-  geom_point(data = uganda_clean,
-             mapping = aes(x = lon_deg,
-                           y = lat_deg,
-                           color = water_source)) +
-  geom_polygon(data = uganda_map,
-               mapping = aes(x = long,
-                             y = lat,
-                             group = group),
-               fill = NA,
-               color = fontcolor) +
-  coord_map(projection = "albers",
-            lat0 = 1,
-            lat1 = 4) +
-  facet_wrap(~ bin_install_year)
+ggplot(data = water_alluvial %>% filter(install_year >= 1950),
+       mapping = aes(x = install_year,
+                     y = pct,
+                     alluvium = water_source,
+                     fill = water_source)) +
+  geom_alluvium() +
+  scale_fill_brewer()
+
+ggplot(data = water_alluvial %>% filter(install_year >= 1950),
+       mapping = aes(x = install_year,
+                     y = pct,
+                     fill = water_source)) +
+  geom_area(color = "black") 
