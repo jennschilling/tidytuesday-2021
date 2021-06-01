0ca6d2e23cbb97377e19fe7cafa4450815df2661
Jenn Schilling <jathorne2@gmail.com>
2021-06-01 09:56:38 -0700

    week 20 - initial commit

---
+++ 2021-05-11/2021-05-11.R
@@ -0,0 +1,12 @@
+# Author : Jenn Schilling
+# Title: #TidyTuesday Broadband in the U.S.
+# Date: May 11 2021
+
+#### Libraries ####
+
+library(tidyverse)
+library(extrafont)
+
+#### Data #### 
+
+broadband <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-05-11/broadband.csv')
