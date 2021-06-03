f19ea0ece0d9d8af4e0ccdb27f2e14cb61e8fe45
Jenn Schilling <jathorne2@gmail.com>
2021-06-03 12:26:45 -0700

    week 21 - initial commit

---
+++ 2021-05-18/2021-05-18.R
@@ -0,0 +1,39 @@
+# Author : Jenn Schilling
+# Title: #TidyTuesday Ask a Manager Survey
+# Date: May 18 2021
+
+#### Libraries ####
+
+library(tidyverse)
+library(extrafont)
+library(ggtext)
+
+#### Data #### 
+
+survey <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-05-18/survey.csv')
+
+#### Formatting ####
+
+font <- "Gill Sans MT"
+fontcolor <- "gray30"
+
+theme_set(theme_minimal(base_size = 12, base_family = font))
+
+theme_update(
+  panel.grid.minor = element_blank(),
+  
+  axis.title = element_text(size = 10, color = fontcolor),
+  axis.text = element_text(size = 9, color = fontcolor),
+  
+  strip.text = element_text(size = 10, color = fontcolor, hjust = 0),
+  
+  plot.title.position = "plot",
+  plot.title = element_markdown(size = 12, color = fontcolor),
+  
+  plot.subtitle = element_markdown(size = 10, color = fontcolor),
+  
+  plot.caption.position = "plot",
+  plot.caption = element_markdown(size = 8, color = fontcolor),
+  
+  plot.margin = margin(t = 10, r = 10, b = 10, l = 10)
+)
