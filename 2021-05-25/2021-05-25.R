c621d2b98bbe2490b08e0681d434df7263f072ff
jaschilling <jaschilling@arizona.edu>
2021-08-20 14:42:32 -0700

    week 22 - custom font added

--- 2021-05-25/2021-05-25.R
+++ 2021-05-25/2021-05-25.R
@@ -12,25 +12,23 @@ library(ggtext)
 
 #### Data #### 
 
-records <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-05-25/records.csv')
+records <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-05-25/records.csv')
+drivers <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-05-25/drivers.csv') %>% 
+  filter(!is.na(records))
 
 #### Formatting ####
 
-font <- "Gill Sans MT"
+font <- "Super Mario 256"
 fontcolor <- "gray30"
 
 theme_set(theme_minimal(base_size = 12, base_family = font))
 
 theme_update(
   panel.grid.minor = element_blank(),
-  panel.grid.major = element_blank(),
+#  panel.grid.major = element_blank(),
   
   axis.title = element_text(size = 10, color = fontcolor),
-  axis.text.x = element_text(size = 9, color = fontcolor),
-  axis.text.y = element_blank(),
-  
-  axis.line.x = element_blank(),
-  axis.ticks.x = element_line(color = fontcolor),
+  axis.text = element_text(size = 9, color = fontcolor),
   
   strip.text = element_text(size = 10, color = fontcolor, hjust = 0),
   
@@ -47,3 +45,9 @@ theme_update(
   
   plot.margin = margin(t = 10, r = 10, b = 10, l = 10)
 )
+
+
+ggplot(data = drivers,
+       mapping = aes(x = year,
+                     y = records)) +
+  geom_point()
