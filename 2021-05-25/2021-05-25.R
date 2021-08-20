b4a85247707d32aa21ae4cfca4bad2aba4770c5d
jaschilling <jaschilling@arizona.edu>
2021-08-20 16:50:57 -0700

    week 22 - final plot

--- 2021-05-25/2021-05-25.R
+++ 2021-05-25/2021-05-25.R
@@ -6,7 +6,6 @@
 
 library(tidyverse)
 library(forcats)
-library(scales)
 library(extrafont)
 library(ggtext)
 
@@ -24,7 +23,7 @@ drivers <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesd
 # loadfonts(device = "win")
 
 font <- "Super Mario 256"
-fontcolor <- "gray30"
+fontcolor <- "black"
 
 theme_set(theme_minimal(base_size = 12, base_family = font))
 
@@ -32,10 +31,15 @@ theme_update(
   panel.grid.minor = element_blank(),
   panel.grid.major = element_blank(),
   
-  axis.title = element_text(size = 10, color = fontcolor),
-  axis.text = element_text(size = 9, color = fontcolor),
-  axis.line = element_line(color = fontcolor),
-  axis.ticks = element_line(color = fontcolor),
+  axis.title.x = element_text(size = 10, color = fontcolor),
+  axis.text.x = element_text(size = 9, color = fontcolor),
+  axis.line.x = element_line(color = fontcolor),
+  axis.ticks.x = element_line(color = fontcolor),
+  
+  axis.title.y = element_blank(),
+  axis.text.y = element_blank(),
+  axis.line.y = element_blank(),
+  axis.ticks.y = element_blank(),
   
   strip.text = element_text(size = 10, color = fontcolor, hjust = 0),
   
@@ -43,7 +47,7 @@ theme_update(
   legend.title = element_text(size = 10, color = fontcolor),
   
   plot.title.position = "plot",
-  plot.title = element_markdown(size = 12, color = fontcolor),
+  plot.title = element_markdown(size = 14, color = fontcolor),
   
   plot.subtitle = element_markdown(size = 10, color = fontcolor),
   
@@ -62,21 +66,64 @@ mario_colors <- c("#A60815", "#193073", "#F2E205", "#F2BD1D", "#F21B1B", # Color
 
 ### Plot ###
 
-single_lap_records <- records %>% filter(type == "Single Lap")
+single_lap_records <- records %>% 
+  filter(type == "Single Lap") %>%
+  mutate(track = fct_rev(track))
 
-ggplot(data = single_lap_records,
+single_lap_records_first_last <- single_lap_records %>%
+  group_by(track) %>%
+  summarise(first = min(date),
+            first_time = max(time),
+            last = max(date),
+            last_time = min(time),
+            .groups = "drop")
+
+single_lap_records_remove_first_last <- single_lap_records %>%
+  anti_join(single_lap_records_first_last, by = c("track", "date" = "first", "time" = "first_time")) %>%
+  anti_join(single_lap_records_first_last, by = c("track", "date" = "last", "time" = "last_time"))
+
+ggplot(data = single_lap_records_remove_first_last,
        mapping = aes(x = date,
-                     y = time,
-                     group = track)) +
-  geom_line(color = fontcolor,
-            size = 1) +
-  facet_wrap(~track,
-             scales = "free_y")
+                     y = track)) +
+  geom_segment(data = single_lap_records_first_last,
+               mapping = aes(x = first,
+                             xend = last,
+                             y = track,
+                             yend = track),
+               color = fontcolor,
+               alpha = 0.4) +
+  geom_tile(mapping = aes(height = 0.3),
+            color = mario_colors[6]) +
+  geom_point(data = single_lap_records_first_last,
+             mapping = aes(x = first,
+                           y = track),
+             color = mario_colors[5],
+             size = 2) +
+  geom_point(data = single_lap_records_first_last,
+             mapping = aes(x = last,
+                           y = track),
+             color = mario_colors[5],
+             size = 2) +
+  geom_text(data = single_lap_records_first_last,
+            mapping = aes(x = min(first),
+                          y = track,
+                          label = track),
+            family = font,
+            color = mario_colors[4],
+            vjust = -1,
+            hjust = 0) +
+  coord_cartesian(clip = "off") +
+  labs(title = "Timeline by Track for Single Lap Mario Kart 64 World Records",
+       subtitle = "There were a lot of world records in the late 1990s and early 2000s.<br>Since then the pace of new records has slowed.",
+       x = "",
+       y = "",
+       caption = "**Data:** Mario Kart World Records | **Design**: Jenn Schilling")
 
-set.seed(123)
-ggplot(data = single_lap_records,
-       mapping = aes(x = record_duration,
-                     y = fct_rev(track))) +
-  geom_jitter(alpha = 0.5,
-              color = fontcolor,
-              height = 0.2)
+ggsave("2021-05-25\\mario_kart.png",
+       plot = last_plot(),
+       device = "png",
+       type = "cairo", 
+       width = 8,
+       height = 10,
+       dpi = 300)
+
