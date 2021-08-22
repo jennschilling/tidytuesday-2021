870d2ff81e3b1b7441d1498b5a2ffd7184105fa4
Jenn Schilling <jathorne2@gmail.com>
2021-08-21 17:40:40 -0700

    week 23 - plot formatting

--- 2021-06-01/2021-06-01.R
+++ 2021-06-01/2021-06-01.R
@@ -27,6 +27,7 @@ theme_update(
   
   axis.title = element_text(size = 10, color = fontcolor),
   axis.text = element_text(size = 9, color = fontcolor),
+  axis.ticks = element_line(color = fontcolor),
   
   strip.text = element_text(size = 10, color = fontcolor, hjust = 0),
   
@@ -46,12 +47,31 @@ theme_update(
 
 #### Plot ####
 
+# Count how many seasons had higher finale viewership
+summary %>%
+  mutate(higher_finale = viewers_finale > viewers_premier) %>%
+  count(higher_finale) 
+# 21 had higher finale viewership, 19 had higher premier viewership
+
 ggplot(data = summary) +
-  geom_point(mapping = aes(x = premiered,
-                           y = viewers_premier)) +
-  geom_point(mapping = aes(x = ended,
-                           y = viewers_finale)) +
   geom_segment(mapping = aes(x = premiered,
                              xend = ended,
                              y = viewers_premier,
-                             yend = viewers_finale))
+                             yend = viewers_finale),
+               color = fontcolor) +
+  geom_point(mapping = aes(x = premiered,
+                           y = viewers_premier),
+             color = "#48D1CC") +
+  geom_point(mapping = aes(x = ended,
+                           y = viewers_finale),
+             color = "#F77613") +
+  scale_y_continuous(labels = number_format(suffix = "M")) +
+  labs(subtitle = '<b style="color:#48D1CC">Season Premier</b> <b style="color:#F77613">Season Finale</b>',
+       y = "Number of Viewers",
+       x = "Show Date",
+       caption = "<b>Data:</b> SurvivoR package | <b>Design:</b> Jenn Schilling")
+
+# Add annotations to the plot
+# Maybe label points and remove y-axis
+
+
