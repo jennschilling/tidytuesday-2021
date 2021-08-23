ff3f4922946db9313d78a98b0d1e8c89d52c7075
Jenn Schilling <jathorne2@gmail.com>
2021-08-22 17:07:57 -0700

    week 24 - initial commit

---
+++ 2021-06-08/2021-06-08.R
@@ -0,0 +1,50 @@
+# Author : Jenn Schilling
+# Title: #TidyTuesday Great Lakes Fish
+# Date: June 8 2021
+
+#### Libraries ####
+
+library(tidyverse)
+library(forcats)
+library(scales)
+library(extrafont)
+library(ggtext)
+
+#### Data #### 
+
+fishing <- readr('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-06-08/fishing.csv')
+
+#### Formatting ####
+
+font <- "Gill Sans MT"
+fontcolor <- "gray30"
+
+theme_set(theme_minimal(base_size = 12, base_family = font))
+
+theme_update(
+  panel.grid.minor = element_blank(),
+  panel.grid.major = element_blank(),
+  
+  panel.background = element_rect(color = "white"),
+  
+  axis.title = element_text(size = 10, color = fontcolor),
+  axis.text = element_text(size = 9, color = fontcolor),
+  axis.ticks = element_line(color = fontcolor),
+  
+  strip.text = element_text(size = 10, color = fontcolor, hjust = 0),
+  
+  legend.text = element_text(size = 10, color = fontcolor),
+  legend.title = element_text(size = 10, color = fontcolor),
+  
+  plot.title.position = "plot",
+  plot.title = element_markdown(size = 12, color = fontcolor),
+  
+  plot.subtitle = element_markdown(size = 10, color = fontcolor),
+  
+  plot.caption.position = "plot",
+  plot.caption = element_markdown(size = 8, color = fontcolor),
+  
+  plot.margin = margin(t = 10, r = 10, b = 10, l = 10)
+)
+
+#### Plot ####
