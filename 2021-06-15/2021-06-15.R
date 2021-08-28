09d00f2117408ba7f7d412c9b63a050a7c0a320d
Jenn Schilling <jathorne2@gmail.com>
2021-08-28 15:50:23 -0700

    week 25 - final plot

--- 2021-06-15/2021-06-15.R
+++ 2021-06-15/2021-06-15.R
@@ -5,31 +5,37 @@
 #### Libraries ####
 
 library(tidyverse)
-library(forcats)
-library(scales)
 library(extrafont)
 library(ggtext)
+library(lubridate)
 
 #### Data #### 
 
 tweets <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-06-15/tweets.csv')
 
-#### Formatting ####
+#### Format ####
 
-font <- "Gill Sans MT"
-fontcolor <- "gray30"
+title_font <- "Consolas"
+axis_font <- "Microsoft Sans Serif" 
 
-theme_set(theme_minimal(base_size = 12, base_family = font))
+background <- "#e5d4c3"
+grid_line <- "#dab198"
+fontcolor <- "gray40"
+
+theme_set(theme_minimal(base_size = 12, base_family = axis_font))
 
 theme_update(
   panel.grid.minor = element_blank(),
-  panel.grid.major = element_blank(),
+  panel.grid.major = element_line(color = grid_line),
   
-  panel.background = element_rect(color = "white"),
+  plot.background = element_rect(fill =  background, color = NA),
+  panel.background = element_rect(fill = background, color = NA),
+  panel.border = element_rect(fill = NA, color = fontcolor),
   
   axis.title = element_text(size = 10, color = fontcolor),
   axis.text = element_text(size = 9, color = fontcolor),
-  axis.ticks = element_line(color = fontcolor),
+  axis.ticks = element_blank(),
+  axis.line = element_blank(),
   
   strip.text = element_text(size = 10, color = fontcolor, hjust = 0),
   
@@ -37,12 +43,48 @@ theme_update(
   legend.title = element_text(size = 10, color = fontcolor),
   
   plot.title.position = "plot",
-  plot.title = element_markdown(size = 12, color = fontcolor),
+  plot.title = element_markdown(size = 14, color = "black", 
+                                face = "bold", family = title_font,
+                                hjust = 0.5),
   
-  plot.subtitle = element_markdown(size = 10, color = fontcolor),
+  plot.subtitle = element_markdown(size = 10, family = title_font),
   
   plot.caption.position = "plot",
-  plot.caption = element_markdown(size = 8, color = fontcolor),
+  plot.caption = element_markdown(size = 8),
   
-  plot.margin = margin(t = 10, r = 10, b = 10, l = 10)
+  plot.margin = margin(t = 10, r = 25, b = 10, l = 25)
 )
+
+#### Aggregate ####
+
+agg_tweets <- tweets %>%
+  mutate(date = date(datetime)) %>%
+  group_by(date) %>%
+  summarise(n = n(),
+            .groups = "drop") %>%
+  filter(!is.na(date))
+
+#### Plot ####
+
+ggplot(data = agg_tweets,
+       mapping = aes(x = date,
+                     y = n,
+                     group = 1)) +
+  geom_line(size = 1) +
+  scale_y_continuous(limits = c(0, 80),
+                     breaks = seq(0, 80, 10)) +
+  scale_x_date(date_breaks = "2 weeks",
+               date_labels = "%b %d") +
+  coord_cartesian(expand = FALSE) +
+  labs(title = "#DUBOISCHALLENGE TWEETS BY DAY",
+       subtitle = "",
+       x = "",
+       y = "",
+       caption = "<b>Data:</b> #DuBoisChallenge Tweets | <b>Design:</b> Jenn Schilling")
+
+ggsave("2021-06-15\\duboischallenge.png",
+       plot = last_plot(),
+       device = "png",
+       width = 7,
+       height = 5,
+       type = "cairo")
