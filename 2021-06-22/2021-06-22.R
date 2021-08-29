ac91810325fa30baa83cd23b837d183dbbc16935
Jenn Schilling <jathorne2@gmail.com>
2021-08-28 17:04:09 -0700

    week 26 - sub plots created

--- 2021-06-22/2021-06-22.R
+++ 2021-06-22/2021-06-22.R
@@ -7,15 +7,45 @@
 library(tidyverse)
 library(extrafont)
 library(ggtext)
+library(ggbump)
+library(scales)
+library(patchwork)
 
 #### Data #### 
 
 parks <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-06-22/parks.csv')
 
+parks <- parks %>%
+  # Make D.C. same city name
+  mutate(city = ifelse(city == "Washington, DC",
+                       "Washington, D.C.",
+                       city))
+
+tucson <- parks %>%
+  filter(city == "Tucson") %>%
+  mutate(park_pct_city_data = parse_number(park_pct_city_data) / 100,
+         pct_near_park_data = parse_number(pct_near_park_data) / 100,
+         spend_per_resident_data = parse_number(spend_per_resident_data))
+
 #### Formatting ####
 
 font <- "Gill Sans MT"
 fontcolor <- "gray30"
+bcolor <- "#F0F0F0"
+
+# Color Palette Sources: 
+# https://www.color-hex.com/color-palette/37104
+# https://i.pinimg.com/originals/71/6e/e6/716ee6bf061d67c8f223ac80a0f1f087.png
+tucson_colors <- c("#FF3E7C", #pink
+                   "#FFAF84", #orange
+                   "#C7BBB5", #grey
+                   "#6F62A9", #purple
+                   "#CF95B4", #mauve
+                   "#152056", #dark blue
+                   "#f8a4b1", #pink
+                   "#f08125", #golden
+                   "#fd7e49", #peach
+                   "#c03e34") #rust
 
 theme_set(theme_minimal(base_size = 12, base_family = font))
 
@@ -23,7 +53,8 @@ theme_update(
   panel.grid.minor = element_blank(),
   panel.grid.major = element_blank(),
   
-  panel.background = element_rect(color = "white"),
+  panel.background = element_rect(fill = bcolor, color = NA),
+  plot.background = element_rect(fill = bcolor, color = NA),
   
   axis.title = element_text(size = 10, color = fontcolor),
   axis.text = element_text(size = 9, color = fontcolor),
@@ -44,3 +75,67 @@ theme_update(
   
   plot.margin = margin(t = 10, r = 10, b = 10, l = 10)
 )
+
+#### Plot ####
+
+rank <- ggplot(data = tucson,
+       mapping = aes(x = year,
+                     y = rank)) +
+  geom_bump(size = 1,
+            color = tucson_colors[1]) +
+  geom_point(size = 2,
+             color = tucson_colors[1]) +
+  scale_y_reverse() +
+  labs(title = "Tucson's rank among the top 100 cities has declined since 2012.",
+       x = "",
+       y = "")
+
+parkarea <- ggplot(data = tucson,
+       mapping = aes(x = year,
+                     y = park_pct_city_data,
+                     group = 1)) +
+  geom_line(size = 1,
+            color = tucson_colors[4]) +
+  scale_y_continuous(limits = c(0.01, 0.04),
+                     labels = percent_format(accuracy = 0.1)) +
+  labs(title = "Parkland as percentage of city area in Tucson is quite low. It increased slightly in 2016 and has remained constant since.",
+       x = "",
+       y = "")
+
+pctresidents <- ggplot(data = tucson,
+       mapping = aes(x = year,
+                     y = pct_near_park_data,
+                     group = 1)) +
+  geom_line(size = 1,
+            color = tucson_colors[10]) +
+  scale_y_continuous(limits = c(0.5, 0.65),
+                     labels = percent_format(accuracy = 0.1)) +
+  labs(title = "More than half of Tucson residents live within a 10 minute walk of a park.",
+       x = "",
+       y = "")
+
+spend <- ggplot(data = tucson,
+       mapping = aes(x = year,
+                     y = spend_per_resident_data,
+                     group = 1)) +
+  geom_line(size = 1,
+            color = tucson_colors[9]) +
+  scale_y_continuous(limits = c(50, 105),
+                     labels = dollar_format(accuracy = 1)) +
+  labs(title = "Spending per resident on parks has declined in Tucson since 2013. Spending has almost been cut in half from 2012 to 2020.",
+       x = "",
+       y = "")
+
+size <- ggplot(data = tucson,
+       mapping = aes(x = year,
+                     y = med_park_size_data,
+                     group = 1)) +
+  geom_line(size = 1,
+            color = tucson_colors[6]) +
+  scale_y_continuous(limits = c(4, 5.5)) +
+  labs(title = "Median park size (in acres) in Tucson has increased by 1.4 acres from 2012 to 2020.",
+       x = "",
+       y = "")
+
+
+
