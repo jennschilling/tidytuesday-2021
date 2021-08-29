e971eb32309cd6a56132be443d86d681d3b8fb91
Jenn Schilling <jathorne2@gmail.com>
2021-08-28 18:42:52 -0700

    made 2021 view wide; added latest 4 plots

--- 2021-all-plots/combine-plots.R
+++ 2021-all-plots/combine-plots.R
@@ -7,7 +7,7 @@
 library(magick)
 library(here)
 
-#### Get Plots for 2021 ####
+#### Get Plots for 2021 [Long] ####
 
 row_1_21 <- image_append(
   c(image_border(image_resize(image_read("2021-01-12\\turner_oil_colors_circle.png"), "500x400!"), color = "white", geometry = "10x10"),
@@ -43,17 +43,71 @@ row_5_21 <- image_append(
 row_6_21 <- image_append(
   c(image_border(image_resize(image_read("2021-05-11\\az_broadband.png"), "500x400!"), color = "white", geometry = "10x10"),
     image_border(image_resize(image_read("2021-05-18\\us_salaries.png"), "500x400!"), color = "white", geometry = "10x10"),
-    image_border(image_resize(image_read("2021-05-25\\mario_kart.png"), "500x400!"), color = "white", geometry = "10x10")))
+    image_border(image_resize(image_read("2021-05-25\\mario_kart.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("2021-06-01\\survivor.png"), "500x400!"), color = "white", geometry = "10x10")))
+
+row_7_21 <- image_append(
+  c(image_border(image_resize(image_read("2021-06-08\\greatlakes.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("2021-06-15\\duboischallenge.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("2021-06-22\\tucsonparks.png"), "500x400!"), color = "white", geometry = "10x10")))
 
 final_21 <- image_append(c(row_1_21,
                            row_2_21,
                            row_3_21,
                            row_4_21,
                            row_5_21,
-                           row_6_21),
+                           row_6_21,
+                           row_7_21),
                          stack = TRUE)
 
 image_write(final_21,
+            "2021-all-plots\\2021-summary.png")
+
+
+#### Get Plots for 2021 [Wide] ####
+
+row_1_21 <- image_append(
+  c(image_border(image_resize(image_read("2021-01-12\\turner_oil_colors_circle.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("2021-01-19\\county_female_map.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("2021-01-19\\county_female_bar.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("2021-01-26\\sankey_plastic.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("2021-02-02\\bach_hs_attain_women.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("2021-02-02\\bach_hs_attain.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("2021-02-09\\wealth_distribution.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("2021-02-16\\c1_final.png"), "500x400!"), color = "white", geometry = "10x10")))
+
+row_2_21 <- image_append(
+  c(image_border(image_resize(image_read("2021-02-16\\c2_final.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("2021-02-23\\earnings_age_2020.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("2021-02-23\\occupations_2020.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("2021-03-02\\superbowl_2.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("2021-03-09\\genre_summary.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("2021-03-16\\steam_games.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("2021-03-23\\un_votes.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("2021-03-30\\foundation_shades.png"), "500x400!"), color = "white", geometry = "10x10")))
+
+row_3_21 <- image_append(
+  c(image_border(image_resize(image_read("2021-04-06\\percent_forest.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("2021-04-20\\netflix.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("2021-04-27\\ceo.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("2021-05-04\\water_art.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("2021-05-11\\az_broadband.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("2021-05-18\\us_salaries.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("2021-05-25\\mario_kart.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("2021-06-01\\survivor.png"), "500x400!"), color = "white", geometry = "10x10")))
+
+row_4_21 <- image_append(
+  c(image_border(image_resize(image_read("2021-06-08\\greatlakes.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("2021-06-15\\duboischallenge.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("2021-06-22\\tucsonparks.png"), "500x400!"), color = "white", geometry = "10x10")))
+
+final_21_w <- image_append(c(row_1_21,
+                             row_2_21,
+                             row_3_21,
+                             row_4_21),
+                           stack = TRUE)
+
+image_write(final_21_w,
             "2021-all-plots\\2021-summary.png")
 
 
@@ -116,7 +170,6 @@ row_2 <- image_append(
     image_border(image_resize(image_read("2021-01-12\\turner_oil_colors_circle.png"), "500x400!"), color = "white", geometry = "10x10"),
     image_border(image_resize(image_read("2021-01-19\\county_female_map.png"), "500x400!"), color = "white", geometry = "10x10")))
 
-
 row_3 <- image_append(
   c(image_border(image_resize(image_read("2021-01-19\\county_female_bar.png"), "500x400!"), color = "white", geometry = "10x10"),
     image_border(image_resize(image_read("2021-01-26\\sankey_plastic.png"), "500x400!"), color = "white", geometry = "10x10"),
@@ -125,7 +178,6 @@ row_3 <- image_append(
     image_border(image_resize(image_read("2021-02-09\\wealth_distribution.png"), "500x400!"), color = "white", geometry = "10x10"),
     image_border(image_resize(image_read("2021-02-16\\c1_final.png"), "500x400!"), color = "white", geometry = "10x10"),
     image_border(image_resize(image_read("2021-02-16\\c2_final.png"), "500x400!"), color = "white", geometry = "10x10")))
-
 
 row_4 <- image_append(
   c(image_border(image_resize(image_read("2021-02-23\\earnings_age_2020.png"), "500x400!"), color = "white", geometry = "10x10"),
@@ -145,11 +197,18 @@ row_5 <- image_append(
     image_border(image_resize(image_read("2021-05-18\\us_salaries.png"), "500x400!"), color = "white", geometry = "10x10"),
     image_border(image_resize(image_read("2021-05-25\\mario_kart.png"), "500x400!"), color = "white", geometry = "10x10")))
 
+row_6 <- image_append(
+  c(image_border(image_resize(image_read("2021-06-01\\survivor.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("2021-06-08\\greatlakes.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("2021-06-15\\duboischallenge.png"), "500x400!"), color = "white", geometry = "10x10"),
+    image_border(image_resize(image_read("2021-06-22\\tucsonparks.png"), "500x400!"), color = "white", geometry = "10x10")))
+
 final_together <- image_append(c(row_1,
                                  row_2,
                                  row_3,
                                  row_4,
-                                 row_5),
+                                 row_5,
+                                 row_6),
                                stack = TRUE)
 
 image_write(final_together,
