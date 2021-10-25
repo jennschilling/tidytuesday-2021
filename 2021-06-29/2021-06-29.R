11cdd60a70aa274b04c2820c45315ab0739587ec
Jenn Schilling <jathorne2@gmail.com>
2021-10-25 12:09:15 -0700

    week 27 - finished plot

--- 2021-06-29/2021-06-29.R
+++ 2021-06-29/2021-06-29.R
@@ -8,6 +8,7 @@ library(tidyverse)
 library(extrafont)
 library(ggtext)
 library(scales)
+library(ggimage)
 
 #### Data #### 
 
@@ -18,9 +19,24 @@ animal_rescues <- read_csv('https://raw.githubusercontent.com/rfordatascience/ti
   filter(cal_year < 2021)
 
 
+# Image Source: http://phylopic.org/image/23cd6aa4-9587-4a2e-8e26-de42885004c9/
+# Alternate Image source: https://flyclipart.com/cat-face-png-clipart-head-winging-black-cat-face-clipart-555323#
+
+prop_animal_rescues <- animal_rescues %>%
+  count(cal_year, animal_group_parent) %>%
+  group_by(cal_year) %>%
+  mutate(total = sum(n),
+         prop = n / total,
+         prop_label = percent(prop, accuracy = 1),
+         img =  "2021-06-29\\PhyloPic.23cd6aa4.David-Orr.Felis-silvestris-catus.png" 
+         #2021-06-29\\cat-face-png-clipart-head-winging-555323.jpg"
+         ) %>%
+  ungroup() 
+
 #### Formatting ####
 
-font <- "Gill Sans MT"
+font <- "Trebuchet MS"
+title_font <- "Candara"
 fontcolor <- "gray30"
 bcolor <- "#F8F8F8"
 
@@ -45,44 +61,62 @@ theme_update(
   legend.title = element_text(size = 10, color = fontcolor),
   
   plot.title.position = "plot",
-  plot.title = element_markdown(size = 12, color = fontcolor),
+  plot.title = element_markdown(size = 12, color = fontcolor, family = title_font),
   
   plot.subtitle = element_markdown(size = 10, color = fontcolor),
   
   plot.caption.position = "plot",
   plot.caption = element_markdown(size = 8, color = fontcolor),
   
-  plot.margin = margin(t = 10, r = 10, b = 10, l = 10)
+  plot.margin = margin(t = 10, r = 20, b = 10, l = 20)
 )
 
 #### Plot ####
 
-top_10_animals <- animal_rescues %>%
-  group_by(animal_group_parent) %>%
-  summarise(n = n(),
-            .groups = "drop") %>%
-  top_n(n = 10) %>%
-  select(animal_group_parent)
+ggplot(data = prop_animal_rescues %>%
+         filter(animal_group_parent == "Cat")) +
+    geom_image(mapping = aes(x = cal_year,
+                             y = cal_year,
+                             image = img,
+                             color = prop),
+               size = 1, 
+               asp = 1)+
+  scale_size_identity() +
+  geom_text(mapping = aes(x = cal_year,
+                          y = cal_year,
+                          label = prop_label),
+            color = "#FFFFFF",
+            family = font,
+            size = 5,
+            hjust = -0.5,
+            vjust = 1.5) +
+  scale_color_gradient(low = "#67a9cf",
+                      high = "#016c59") +
+  guides(color = "none") +
+  facet_wrap(~ cal_year,
+             scales = "free",
+             strip.position = "top") +
+  labs(title = "Cats account for about half of all animals rescued each year by the London Fire Brigade<br>",
+       x = "",
+       y = "",
+       caption = "<b>Data:</b> London.gov | <b>Image:</b> PhyloPic | <b>Design:</b> Jenn Schilling") +
+  theme(axis.line = element_blank(),
+        axis.ticks = element_blank(),
+        axis.text = element_blank(),
+        strip.text = element_text(size = 14, 
+                                  color = fontcolor, 
+                                  hjust = 0.5),
+        plot.title = element_markdown(size = 20, 
+                                      color = fontcolor, 
+                                      family = title_font, 
+                                      margin = margin(0, 0, 20, 0)),
+        panel.spacing = unit(2, "lines"))
 
-ggplot(data = animal_rescues %>%
-         right_join(., top_10_animals, by = "animal_group_parent"),
-       mapping = aes(x = incident_notional_cost,
-                     y = fct_rev(animal_group_parent),
-                     color = animal_group_parent)) +
-  geom_boxplot()
 
-
-ggplot(data = animal_rescues %>%
-         right_join(., top_10_animals, by = "animal_group_parent") %>%
-         group_by(cal_year, animal_group_parent) %>%
-         summarise(n = n(),
-                   .groups = "drop"),
-       mapping = aes(x = cal_year,
-                     y = n,
-                     group = animal_group_parent,
-                     color = animal_group_parent)) +
-  geom_line() +
-  geom_point() +
-  facet_wrap(~ animal_group_parent,
-             ncol = 1) +
-  guides(color = FALSE)
+# Save
+ggsave("2021-06-29\\cat_rescues.png",
+       plot = last_plot(),
+       device = "png",
+       width = 12,
+       height = 8,
+       type = "cairo")
