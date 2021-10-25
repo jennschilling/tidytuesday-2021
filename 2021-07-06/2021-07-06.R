c640839c9cf8c4b55a340e0fafcfdda1560646f6
Jenn Schilling <jathorne2@gmail.com>
2021-10-25 14:10:37 -0700

    week 28 - finished plot

--- 2021-07-06/2021-07-06.R
+++ 2021-07-06/2021-07-06.R
@@ -19,22 +19,23 @@ holidays <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytues
 cal <- tibble(
   date = seq(ymd(20200101), ymd(20201231), by = 1)
 ) %>%
-  mutate(month = month(date, label = TRUE),
+  mutate(month = month(date, label = TRUE, abbr = TRUE),
          day = day(date),
          week = as.numeric(format(date, "%U"))) %>%
   left_join(holidays,
             by = c("month", "day")) %>%
   mutate(holiday = ifelse(is.na(date_of_holiday), 0, 1)) %>%
+  group_by(date, month, week, day, weekday) %>%
+  summarise(count_holidays = sum(holiday),
+            .groups = "drop") %>%
   mutate(weekday = weekdays(date),
+         month = month(date, label = TRUE, abbr = FALSE),
          weekday = factor(weekday, c("Sunday", "Monday", "Tuesday",
                                      "Wednesday", "Thursday", "Friday",
                                      "Saturday")),
-         month = factor(month, c("Jan", "Feb", "Mar", "Apr",
-                                 "May", "Jun", "Jul", "Aug",
-                                 "Sep", "Oct", "Nov", "Dec"))) %>%
-  group_by(date, month, week, day, weekday) %>%
-  summarise(count_holidays = sum(holiday),
-            .groups = "drop") 
+         month = factor(month, c("January", "February", "March", "April",
+                                 "May", "June", "July", "August",
+                                 "September", "October", "November", "December")))
 
 #### Formatting ####
 
@@ -66,7 +67,7 @@ theme_update(
   plot.title.position = "plot",
   plot.title = element_markdown(size = 12, color = fontcolor, family = title_font),
   
-  plot.subtitle = element_markdown(size = 10, color = fontcolor),
+  plot.subtitle = element_markdown(size = 11, color = fontcolor, family = title_font),
   
   plot.caption.position = "plot",
   plot.caption = element_markdown(size = 8, color = fontcolor),
@@ -81,8 +82,30 @@ ggplot(data = cal,
                      y = week,
                      fill = count_holidays,
                      label = day)) +
-  geom_tile() +
-  geom_text() +
+  geom_tile(color = fontcolor) +
+  geom_text(family = font) +
   facet_wrap(~ month,
              scales = "free_y") +
-  scale_y_reverse()
+  scale_y_reverse() +
+  scale_fill_gradient(low = "#FFFFFF",
+                       high = "#c51b8a") +
+  guides(fill = "none") +
+  labs(title = "Worldwide Independence Days",
+       subtitle = "September 15 is an independence day in 5 countries. In 1821, 
+       Costa Rica, El Salvador, Guatemala, Honduras, and Nicaragua gained independence
+       from<br>the Spanish Empire through the Act of Independence of Central America.
+       <br><br>
+       Darker shade means that more countries have an Independence Day on the date.", 
+       x = "",
+       y = "",
+       caption = "<b>Data:</b> Wikipedia | <b>Design:</b> Jenn Schilling") +
+  theme(axis.line = element_blank(),
+        axis.ticks = element_blank(),
+        axis.text = element_blank(),
+        strip.text = element_text(size = 13, 
+                                  color = "#000000"),
+        plot.title = element_markdown(size = 20, 
+                                      color = fontcolor, 
+                                      family = title_font))
+
+        
