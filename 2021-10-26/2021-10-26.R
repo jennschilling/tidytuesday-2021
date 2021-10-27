47f2d3c24c65242389338f526731784f290dc0e9
jaschilling <jaschilling@arizona.edu>
2021-10-27 14:09:30 -0700

    week 44 - initial commit

---
+++ 2021-10-26/2021-10-26.R
@@ -0,0 +1,56 @@
+# Author : Jenn Schilling
+# Title: #TidyTuesday Ultra Trail Running
+# Date: October 26 2021
+
+#### Libraries ####
+
+library(tidyverse)
+library(extrafont)
+library(ggtext)
+library(scales)
+
+#### Data #### 
+
+ultra_rankings <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-10-26/ultra_rankings.csv')
+race <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-10-26/race.csv')
+
+#### Formatting ####
+
+font <- "Trebuchet MS"
+title_font <- "Candara"
+fontcolor <- "gray30"
+bcolor <- "#F8F8F8"
+
+theme_set(theme_minimal(base_size = 12, base_family = font))
+
+theme_update(
+  panel.grid.minor = element_blank(),
+  panel.grid.major = element_blank(),
+  
+  panel.background = element_rect(fill = bcolor, color = NA),
+  plot.background = element_rect(fill = bcolor, color = NA),
+  
+  axis.title = element_text(size = 10, color = fontcolor),
+  axis.text = element_text(size = 9, color = fontcolor),
+  axis.ticks = element_line(color = fontcolor),
+  
+  axis.line = element_line(color = fontcolor),
+  
+  strip.text = element_text(size = 10, color = fontcolor, hjust = 0),
+  
+  legend.text = element_text(size = 10, color = fontcolor),
+  legend.title = element_text(size = 10, color = fontcolor),
+  
+  plot.title.position = "plot",
+  plot.title = element_markdown(size = 12, color = fontcolor, family = title_font),
+  
+  plot.subtitle = element_markdown(size = 10, color = fontcolor),
+  
+  plot.caption.position = "plot",
+  plot.caption = element_markdown(size = 8, color = fontcolor),
+  
+  plot.margin = margin(t = 10, r = 10, b = 10, l = 10)
+)
+
+
+#### Plot ####
