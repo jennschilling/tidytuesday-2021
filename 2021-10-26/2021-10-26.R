4fe14803eb6fe623fd1b9548f45f644cc12a2dca
jaschilling <jaschilling@arizona.edu>
2021-10-27 16:00:25 -0700

    week 44 - initial plot created

--- 2021-10-26/2021-10-26.R
+++ 2021-10-26/2021-10-26.R
@@ -37,7 +37,59 @@ w_freq_racers <- freq_racers %>%
   select(runner, n, race_year_id, date, rank, max_age, 
          nationality, time_in_seconds, country, distance) %>%
   mutate(race_year = year(date),
-         race_month = month(date))
+         race_month = month(date)) %>%
+  separate(runner, into = c("lname", "fname"), sep = " ") %>%
+  mutate(lname = str_to_title(lname)) %>%
+  unite("runner", fname:lname, sep = " ") %>%
+  # Add column for top 10 rank
+  mutate(top_10_rank = ifelse(is.na(rank), FALSE, rank <= 10)) %>%
+  # Arrange by age
+  arrange(max_age) %>%
+  mutate(runner = factor(runner, unique(runner))) %>%
+  # Create segment parameters 
+  mutate(
+    x = case_when(
+      country == "United States" | country == "Croatia" ~ date - 15,
+      country == "Sweden" ~ date - 15,
+      country == "France" | country == "Colombia" ~ date,
+      country == "Switzerland" ~ date - 15
+      ),
+    xend = case_when(
+      country == "United States" | country == "Croatia" ~ date + 15,
+      country == "Sweden" ~ date + 15,
+      country == "France" | country == "Colombia" ~ date,
+      country == "Switzerland" ~ date + 15
+    ),
+    y = case_when(
+      country == "United States" | country == "Croatia" ~ distance - distance / 50,
+      country == "Sweden" ~ distance + distance / 50,
+      country == "France" | country == "Colombia" ~ distance - distance / 50,
+      country == "Switzerland" ~ distance
+    ),
+    yend = case_when(
+      country == "United States" | country == "Croatia" ~ distance + distance / 50,
+      country == "Sweden" ~ distance - distance / 50,
+      country == "France" | country == "Colombia"  ~ distance + distance / 50,
+      country == "Switzerland" ~ distance
+    ),
+    
+    x2 = case_when(
+      country == "Croatia"  ~ date - 15,
+      country == "Colombia" ~ date - 15
+    ),
+    xend2 = case_when(
+      country == "Croatia" ~ date + 15,
+      country == "Colombia" ~ date + 15
+    ),
+    y2 = case_when(
+      country == "Croatia" ~ distance + distance / 50,
+      country == "Colombia" ~ distance
+    ),
+    yend2 = case_when(
+      country == "Croatia" ~  distance - distance / 50,
+      country == "Colombia" ~ distance
+    )
+)
 
 #### Formatting ####
 
@@ -61,7 +113,7 @@ theme_update(
   
   axis.line = element_line(color = fontcolor),
   
-  strip.text = element_text(size = 10, color = fontcolor, hjust = 0),
+  strip.text = element_text(size = 12, color = fontcolor, hjust = 0),
   
   legend.text = element_text(size = 10, color = fontcolor),
   legend.title = element_text(size = 10, color = fontcolor),
@@ -79,3 +131,49 @@ theme_update(
 
 
 #### Plot ####
+
+ggplot() +
+  geom_rect(data = w_freq_racers,
+            mapping = aes(xmin = date - 15,
+                          xmax = date + 15,
+                          ymin = distance - distance / 50,
+                          ymax = distance + distance / 50,
+                          color = time_in_seconds),
+            fill = NA,
+            size = 1) +
+  geom_segment(data = w_freq_racers,
+               mapping = aes(x = x,
+                             xend = xend,
+                             y = y,
+                             yend = yend,
+                             color = time_in_seconds),
+                size = 1) +
+  geom_segment(data = w_freq_racers %>% filter(!is.na(x2)),
+               mapping = aes(x = x2,
+                             xend = xend2,
+                             y = y2,
+                             yend = yend2,
+                             color = time_in_seconds),
+               size = 1) +
+  # scale_color_viridis_c(option = "D") +
+  scale_color_gradient2(high = "#787FF6",
+                        mid = "#4ADEDE") +
+  # scale_color_gradient2(high = "#205072",
+  #                       mid = "#56C596") +
+  # scale_color_distiller(palette = "Set2",
+  #                       direction = -1,
+  #                       na.value = "#bdbdbd") +
+  geom_point(data = w_freq_racers %>%
+               filter(top_10_rank),
+             mapping = aes(x = date + 15,
+                           y = distance + distance / 50),
+             color = "#de4a94",
+             shape = 8) +
+  facet_wrap(~ runner,
+             ncol = 1) +
+  guides(color = "none") +
+  labs(y = "",
+       x = "") +
+  theme(axis.ticks.y = element_blank(),
+        axis.line.y = element_blank(),
+        axis.text.y = element_blank())
