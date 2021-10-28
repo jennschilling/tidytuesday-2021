8ab1dfd699fa312ac3c71813492e8762d0a532cd
Jenn Schilling <jathorne2@gmail.com>
2021-10-27 22:04:22 -0700

    week 44 - finished plot; still not happy with layout

--- 2021-10-26/2021-10-26.R
+++ 2021-10-26/2021-10-26.R
@@ -9,6 +9,7 @@ library(extrafont)
 library(ggtext)
 library(scales)
 library(lubridate)
+library(patchwork) # using dev version
 
 #### Data #### 
 
@@ -137,7 +138,10 @@ theme_update(
 # Runners are arranged from youngest to oldest
 # Lines within the rectangle represent the location of the race
 
-ggplot() +
+
+# Create plot
+
+run_plot <- ggplot() +
   geom_rect(data = w_freq_racers,
             mapping = aes(xmin = date - 15,
                           xmax = date + 15,
@@ -183,23 +187,14 @@ ggplot() +
              ncol = 1,
              scales = "free_x",
              strip.position = "left") +
-  scale_x_date(limits = c(as.Date("2012-01-01"), as.Date("2022-01-01")),
+  scale_x_date(limits = c(as.Date("2011-12-01"), as.Date("2022-01-01")),
                date_breaks = "years",
                date_labels = "%Y") +
-  scale_y_continuous(limits = c(145, 185)) +
+#  scale_y_continuous(limits = c(145, 185)) +
   guides(color = "none") +
   coord_cartesian(clip = "off",
                   expand = FALSE) +
-  labs(title = "Each run by the women who have run more than 20 Ultra Trail Runs",
-       subtitle = "Runners are arranged from youngest to oldest; the youngest is 46 and the oldest is 62, as of their last race.<br>
-       The x position along the timeline represents the date of the race from 2012 through 2021.<br>
-       The y position represents the distance of the race: the shortest race was 155km; the longest race was 174.2km.<br>
-       The color represents the runner's time: 
-       <span style = 'color:#ae017e;'>faster times</span>; 
-       <span style = 'color:#fa9fb5;'>slower times</span>; 
-       grey means no time.<br>
-       A <span style = 'color:#01AE7E';>star</span> in the top corner represents a top 10 finish.",
-       y = "",
+  labs(y = "",
        x = "",
        caption = "<b>Data:</b> International Trail Running Association (ITRA) | <b>Design:</b> Jenn Schilling") +
   theme(axis.ticks.y = element_blank(),
@@ -207,14 +202,154 @@ ggplot() +
         axis.text.y = element_blank(),
         panel.spacing = unit(1, "cm"),
         strip.placement.y = "inside",
-        strip.text.y.left = element_text(size = 20, color = fontcolor, 
-                                         angle = 0, vjust = 0, hjust = 1))
+        strip.text.y.left = element_text(size = 18, color = fontcolor, 
+                                         angle = 0, vjust = 0, hjust = 1,
+                                         family = title_font))
+
+
+# Create title, explanation, and legend
+
+legend <- w_freq_racers %>%
+  select(country) %>%
+  unique() %>%
+  mutate(
+    date = 100,
+    distance = 100,
+    
+    x = case_when(
+      country == "United States" | country == "Croatia" ~ date - 15,
+      country == "Sweden" ~ date - 15,
+      country == "France" | country == "Colombia" ~ date,
+      country == "Switzerland" ~ date - 15
+    ),
+    xend = case_when(
+      country == "United States" | country == "Croatia" ~ date + 15,
+      country == "Sweden" ~ date + 15,
+      country == "France" | country == "Colombia" ~ date,
+      country == "Switzerland" ~ date + 15
+    ),
+    y = case_when(
+      country == "United States" | country == "Croatia" ~ distance - distance / 50,
+      country == "Sweden" ~ distance + distance / 50,
+      country == "France" | country == "Colombia" ~ distance - distance / 50,
+      country == "Switzerland" ~ distance
+    ),
+    yend = case_when(
+      country == "United States" | country == "Croatia" ~ distance + distance / 50,
+      country == "Sweden" ~ distance - distance / 50,
+      country == "France" | country == "Colombia"  ~ distance + distance / 50,
+      country == "Switzerland" ~ distance
+    ),
+    
+    x2 = case_when(
+      country == "Croatia"  ~ date - 15,
+      country == "Colombia" ~ date - 15
+    ),
+    xend2 = case_when(
+      country == "Croatia" ~ date + 15,
+      country == "Colombia" ~ date + 15
+    ),
+    y2 = case_when(
+      country == "Croatia" ~ distance + distance / 50,
+      country == "Colombia" ~ distance
+    ),
+    yend2 = case_when(
+      country == "Croatia" ~  distance - distance / 50,
+      country == "Colombia" ~ distance
+    )
+  )
+
+
+legend_plot <- ggplot() +
+  geom_rect(data = legend,
+            mapping = aes(xmin = date - 15,
+                          xmax = date + 15,
+                          ymin = distance - distance / 50,
+                          ymax = distance + distance / 50),
+            fill = NA,
+            color = fontcolor,
+            size = 1) +
+  geom_segment(data = legend,
+               mapping = aes(x = x,
+                             xend = xend,
+                             y = y,
+                             yend = yend),
+               color = fontcolor,
+               size = 1) +
+  geom_segment(data = legend %>% filter(!is.na(x2)),
+               mapping = aes(x = x2,
+                             xend = xend2,
+                             y = y2,
+                             yend = yend2),
+               color = fontcolor,
+               size = 1) +
+  geom_text(data = legend,
+            mapping = aes(x = date + 18,
+                          y = distance,
+                          label = country),
+            family = title_font,
+            color = fontcolor,
+            size = 6,
+            hjust = "left") +
+  facet_wrap(~ country,
+             ncol = 1) +
+  scale_x_continuous(limits = c(80, 200)) +
+  theme_void() +
+  theme(strip.text.x = element_blank())
+
+titles <- tibble(
+  title = "Each run by the women who have run<br>more than 20 Ultra Trail Runs",
+  
+  subtitle = "
+  Runners are arranged from youngest to oldest; the youngest is 46 and the oldest is 62, as of their last race.<br>
+  The x position along the timeline represents the date of the race (2012-2021).<br>
+  The y position represents the distance of the race: the shortest race was 155km; the longest race was 174.2km.<br>
+  The color represents the runner's time: 
+        <span style = 'color:#ae017e;'>faster times</span>; 
+       <span style = 'color:#fa9fb5;'>slower times</span>; 
+       grey means no time.<br>
+  A <span style = 'color:#01AE7E';>star</span> in the top corner represents a Top 10 finish.<br>
+  The style of the box represents the country in which the race was held."
+)
+
+title_plot <- ggplot() +
+  geom_textbox(data = titles,
+               mapping = aes(x = 0,
+                             y = 40,
+                             label = title),
+               fill = NA, 
+               box.color = NA,
+               family = title_font,
+               color = fontcolor,
+               size = 15,
+               width = unit(10, "in")) +
+  geom_textbox(data = titles,
+            mapping = aes(x = 0,
+                          y = 1,
+                          label = subtitle),
+            fill = NA, 
+            box.color = NA,
+            family = title_font,
+            color = fontcolor,
+            size = 8,
+            width = unit(10, "in")) +
+  scale_x_continuous(limits = c(-2, 2)) +
+  scale_y_continuous(limits = c(-30, 45)) +
+  theme_void()
+
+# Put the pieces together
+
+left_side <- title_plot / (plot_spacer() | legend_plot | plot_spacer()) / plot_spacer() +
+  plot_layout(heights = c(2, 0.75, 2),
+              ncol = 1)
+
+left_side | run_plot 
 
 # Save
 ggsave("2021-10-26\\greatracers.png",
        plot = last_plot(),
        device = "png",
-       width = 14,
-       height = 18,
+       width = 25,
+       height = 20,
        type = "cairo")
 
