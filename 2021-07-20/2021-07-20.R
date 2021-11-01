3ded82eb917f76e0d5f6f4a3e23cb6e1da405a24
jaschilling <jaschilling@arizona.edu>
2021-11-01 16:29:37 -0700

    week 30 - finished plot

--- 2021-07-20/2021-07-20.R
+++ 2021-07-20/2021-07-20.R
@@ -8,15 +8,36 @@ library(tidyverse)
 library(extrafont)
 library(ggtext)
 library(scales)
+library(lubridate)
+library(geofacet)
 
 #### Data #### 
 
 drought <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-07-20/drought.csv')
 
+# Put data into a format with months and week numbers
+# Note have data starting in half of July 2001 through half of July 2021
+# Aggregate drought levels
+drought_d <- drought %>%
+  mutate(year_start = year(valid_start),
+         month_start = month(valid_start),
+         week_num_start = week(valid_start)) %>%
+  filter(stat_fmt == 2) %>%
+  select(-area_total, -pop_pct, -pop_total) %>%
+  pivot_wider(names_from = "drought_lvl",
+              values_from = "area_pct") %>%
+  mutate(drought = D0 + D1 + D2 + D3 + D4,
+         no_drought = None) %>%
+  select(state_abb, valid_start, year_start, month_start, week_num_start, drought, no_drought) %>%
+  pivot_longer(drought:no_drought,
+               names_to = "drought",
+               values_to = "area_pct") %>%
+  mutate(drought = factor(drought, levels = c("no_drought", "drought")))
+
 #### Formatting ####
 
 font <- "Trebuchet MS"
-title_font <- "Georgia"
+titlefont <- "Georgia"
 fontcolor <- "gray30"
 bcolor <- "#F8F8F8"
 
@@ -41,12 +62,41 @@ theme_update(
   legend.title = element_text(size = 10, color = fontcolor),
   
   plot.title.position = "plot",
-  plot.title = element_markdown(size = 17, color = fontcolor, family = font),
+  plot.title = element_markdown(size = 20, color = fontcolor, family = titlefont),
   
-  plot.subtitle = element_markdown(size = 11, color = fontcolor, family = font),
+  plot.subtitle = element_markdown(size = 11, color = fontcolor, family = font, lineheight = 1.5),
   
   plot.caption.position = "plot",
   plot.caption = element_markdown(size = 8, color = fontcolor, hjust = 1.04),
   
   plot.margin = margin(t = 15, r = 50, b = 15, l = 30)
 )
+
+#### Plot ####
+
+ggplot(data = drought_d,
+       mapping = aes(x = valid_start,
+                     y = area_pct,
+                     fill = drought)) +
+  geom_area() +
+  scale_fill_manual(values = c(bcolor, "#EE7C09"),
+                    labels = c("No Drought", "Drought")) +
+  guides(fill = "none") +
+  facet_geo(~ state_abb) +
+  labs(title = "Many states are consistently in some level of drought; western states experience the most drought.",
+       subtitle = "<br>Graphs show the percent of the land area in each state that is in <span style = 'color:#EE7C09;'><b>drought</b></span> every week from July 2001 to July 2021.
+       <br>Drought includes every level of drought classification from U.S. Drought Monitor from abnormally dry to exceptional drought.<br>",
+       x = "",
+       y = "",
+       caption = "<br><b>Data:</b> U.S. Drought Monitor | <b>Design:</b> Jenn Schilling") +
+  theme(axis.line = element_blank(),
+        axis.ticks = element_blank(),
+        axis.text = element_blank())
+
+# Save
+ggsave("2021-07-20\\usdrought.png",
+       plot = last_plot(),
+       device = "png",
+       width = 14,
+       height = 8,
+       type = "cairo")
