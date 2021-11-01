c5b7389ca0f50ed0360ad6a45faecf248132bfdd
jaschilling <jaschilling@arizona.edu>
2021-11-01 14:43:40 -0700

    week 30 - initial commit

---
+++ 2021-07-20/2021-07-20.R
@@ -0,0 +1,52 @@
+# Author : Jenn Schilling
+# Title: #TidyTuesday US Drought
+# Date: July 20 2021
+
+#### Libraries ####
+
+library(tidyverse)
+library(extrafont)
+library(ggtext)
+library(scales)
+
+#### Data #### 
+
+drought <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-07-20/drought.csv')
+
+#### Formatting ####
+
+font <- "Trebuchet MS"
+title_font <- "Georgia"
+fontcolor <- "gray30"
+bcolor <- "#F8F8F8"
+
+theme_set(theme_minimal(base_size = 12, base_family = font))
+
+theme_update(
+  panel.grid.minor = element_blank(),
+  panel.grid.major = element_blank(),
+  
+  panel.background = element_rect(fill = bcolor, color = NA),
+  plot.background = element_rect(fill = bcolor, color = NA),
+  
+  axis.title = element_text(size = 10, color = fontcolor),
+  axis.text = element_text(size = 9, color = fontcolor),
+  axis.ticks = element_line(color = fontcolor),
+  
+  axis.line = element_line(color = fontcolor),
+  
+  strip.text = element_text(size = 10, color = fontcolor, hjust = 0),
+  
+  legend.text = element_text(size = 10, color = fontcolor),
+  legend.title = element_text(size = 10, color = fontcolor),
+  
+  plot.title.position = "plot",
+  plot.title = element_markdown(size = 17, color = fontcolor, family = font),
+  
+  plot.subtitle = element_markdown(size = 11, color = fontcolor, family = font),
+  
+  plot.caption.position = "plot",
+  plot.caption = element_markdown(size = 8, color = fontcolor, hjust = 1.04),
+  
+  plot.margin = margin(t = 15, r = 50, b = 15, l = 30)
+)
