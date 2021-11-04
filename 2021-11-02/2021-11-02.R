52fd04da9aee39199d106a6536d68be8fc5fdef6
Jenn Schilling <jathorne2@gmail.com>
2021-11-03 21:04:27 -0700

    week 45 - finished plot

--- 2021-11-02/2021-11-02.R
+++ 2021-11-02/2021-11-02.R
@@ -5,26 +5,43 @@
 #### Libraries ####
 
 library(sf)
-library(raster)
-library(dplyr)
 library(spData)
-# install.packages("spDataLarge", repos = "https://nowosad.github.io/drat/", type = "source")
-library(spDataLarge) 
 library(tmap)   
-library(leaflet) 
 library(ggplot2) 
+library(tidycensus)
+library(extrafont)
+library(scales)
+
+#### Data ####
+
+# B19013_001
+# Estimate Median household income in the past 12 months (in 2019 inflation-adjusted dollars)
+
+mich <- get_acs(state = "Michigan",
+                geography = "county", 
+                variables = "B19013_001", 
+                geometry = TRUE)
 
 #### Plot ####
 
-# From: https://geocompr.robinlovelace.net/adv-map.html
+# Resource: https://walker-data.com/census-r/mapping-census-data-with-r.html
 
-# Add fill layer to nz shape
-tm_shape(nz) +
-  tm_fill() 
-# Add border layer to nz shape
-tm_shape(nz) +
-  tm_borders() 
-# Add fill and border layers to nz shape
-tm_shape(nz) +
-  tm_fill() +
-  tm_borders() 
+tm <- tm_shape(mich) +
+  tm_polygons(col = "estimate",
+              palette = "Blues",
+              legend.format = c(prefix = "$"),
+              title = "") +
+  tm_layout(main.title = "Median Household Income\nby County in Michigan",
+            frame = FALSE,
+            legend.outside = FALSE,
+            legend.hist.width = 5,
+            fontfamily = "Verdana",
+            main.title.fontfamily = "Verdana",
+            main.title.position = "left",
+            main.title.size = 1,
+            attr.outside = TRUE) +
+  tm_credits(text = "Data: 2015-2019 American Community Survey\nDesign: Jenn Schilling",
+             position = "left")
+
+# Save
+tmap_save(tm, "2021-11-02\\michiganincome.png")
