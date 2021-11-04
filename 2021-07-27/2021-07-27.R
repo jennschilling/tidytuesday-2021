9f6cc528bd3448f0a3d813021fb4489b5b64776a
jaschilling <jaschilling@arizona.edu>
2021-11-04 16:54:42 -0700

    week 31 - initial commit

---
+++ 2021-07-27/2021-07-27.R
@@ -0,0 +1,54 @@
+# Author : Jenn Schilling
+# Title: #TidyTuesday Olympics
+# Date: July 27 2021
+
+#### Libraries ####
+
+library(tidyverse)
+library(extrafont)
+library(ggtext)
+library(scales)
+
+#### Data #### 
+
+olympics <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-07-27/olympics.csv')
+
+#### Formatting ####
+
+font <- "Gill Sans MT"
+titlefont <- "Georgia"
+fontcolor <- "gray30"
+bcolor <- "#F8F8F8"
+
+theme_set(theme_minimal(base_size = 12, base_family = font))
+
+theme_update(
+  panel.grid.minor = element_blank(),
+  panel.grid.major = element_blank(),
+  
+  panel.background = element_rect(fill = bcolor, color = NA),
+  plot.background = element_rect(fill = bcolor, color = NA),
+  
+  axis.title = element_text(size = 10, color = fontcolor),
+  axis.text = element_text(size = 9, color = fontcolor),
+  axis.ticks = element_line(color = fontcolor),
+  
+  axis.line = element_line(color = fontcolor),
+  
+  strip.text = element_text(size = 10, color = fontcolor, hjust = 0),
+  
+  legend.text = element_text(size = 10, color = fontcolor),
+  legend.title = element_text(size = 10, color = fontcolor),
+  
+  plot.title.position = "plot",
+  plot.title = element_markdown(size = 20, color = fontcolor, family = titlefont),
+  
+  plot.subtitle = element_markdown(size = 11, color = fontcolor, family = font, lineheight = 1.5),
+  
+  plot.caption.position = "plot",
+  plot.caption = element_markdown(size = 8, color = fontcolor, hjust = 1.04),
+  
+  plot.margin = margin(t = 15, r = 50, b = 15, l = 30)
+)
+
+#### Plot ####
