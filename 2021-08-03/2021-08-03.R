7d3c6ad4fcf385c5cb81ff3c09075baf8cb405d2
Jenn Schilling <jathorne2@gmail.com>
2021-11-07 16:59:58 -0700

    week 32 - finished plot

--- 2021-08-03/2021-08-03.R
+++ 2021-08-03/2021-08-03.R
@@ -8,8 +8,6 @@ library(tidyverse)
 library(extrafont)
 library(ggtext)
 library(ggalluvial)
-library(magick)
-library(grid)
 
 #### Data #### 
 
@@ -20,14 +18,36 @@ sport_events_year <- paralympics %>%
   unique() %>%
   count(year, type) %>%
   group_by(type) %>%
-  mutate(sport_label = ifelse(row_number() == 1, type, NA),
-         sport_label_2 = ifelse(year == 2016, type, NA)) %>%
+  mutate(sport_label = ifelse(row_number() == 1, type, NA)) %>%
   ungroup()
 
+sport_events_labels <- sport_events_year %>%
+  filter(!is.na(sport_label)) %>%
+  mutate(
+    x = case_when(
+      year == 1980 ~ 0.8,
+      year == 1984 ~ 10.2,
+      year == 1996 ~ 10.2,
+      year == 2016 ~ 10.2),
+    y = case_when(
+      type == "Archery" ~ 335,
+      type == "Athletics" ~ 250,
+      type == "Basketball" ~ 162,
+      type == "Fencing" ~ 153,
+      type == "Swimming" ~ 100,
+      type == "Table Tennis" ~ 38,
+      type == "Volleyball" ~ 25,
+      type == "Wheelchair Tennis" ~ 11,
+      type == "Powerlifting" ~ 138,
+      type == "Rugby" ~ 126,
+      type == "Triathlon" ~ 24)
+    )
 
-# Paralympic logo
-logo <- image_read("https://www.paralympic.org/sites/default/files/styles/large_original/public/2019-12/IPC%20logo.jpg?itok=lVrcjjSR")
-logo <- rasterGrob(logo, interpolate = TRUE)
+sport_events_labels_left <- sport_events_labels %>%
+  filter(x == 0.8)
+
+sport_events_labels_right <- sport_events_labels %>%
+  filter(x == 10.2)
 
 #### Formatting ####
 
@@ -57,14 +77,14 @@ theme_update(
   legend.title = element_text(size = 12, color = fontcolor),
   
   plot.title.position = "plot",
-  plot.title = element_markdown(size = 16, color = fontcolor, family = titlefont),
+  plot.title = element_markdown(size = 15, color = fontcolor, family = titlefont),
   
-  plot.subtitle = element_markdown(size = 11, color = fontcolor, family = font, lineheight = 1.5),
+  plot.subtitle = element_markdown(size = 11, color = fontcolor, family = font),
   
   plot.caption.position = "plot",
   plot.caption = element_markdown(size = 8, color = fontcolor, hjust = 1.04),
   
-  plot.margin = margin(t = 20, r = 20, b = 20, l = 20)
+  plot.margin = margin(t = 20, r = 85, b = 20, l = 85)
 )
 
 
@@ -79,28 +99,34 @@ ggplot(data = sport_events_year,
                      y = n)) +
   geom_alluvium(alpha = 0.8) +
   geom_stratum(width = 1/3, alpha = 0, color = NA) +
-  geom_text(data = sport_events_year, #%>%
-              #filter(type %in% c("")),
-            mapping = aes(label = sport_label,
-                          x = as.factor(year)),
-            stat = "stratum",
-            family = font,
-            color = "#FFFFFF",
-            size = 4,
-            hjust = 0.5,
-            nudge_x = 0) +
+  annotate("text",
+           x = sport_events_labels_left$x,
+           y = sport_events_labels_left$y,
+           label = sport_events_labels_left$sport_label,
+           family = font,
+           hjust = 1,
+           size = 4) +
+  annotate("text",
+           x = sport_events_labels_right$x,
+           y = sport_events_labels_right$y,
+           label = sport_events_labels_right$sport_label,
+           family = font,
+           hjust = 0,
+           size = 4) +
   guides(fill = "none") +
-  coord_cartesian(expand = FALSE,
+  coord_trans(expand = FALSE,
                   clip = "off") +
   scale_fill_manual(values = c("#208eb7", "#1c4c5e", "#7ec993", "#1c5f1e", 
                                "#9bc732", "#5826a6", "#bcaff9", "#5064be", 
                                "#cd49dc", "#af2168", "#7b3f5b")) +
   labs(title = "The 1984 Paralympics had the greatest number of events and saw the addition of Powerlifting. Since then, the number<br>
-  <br>of overall events has declined. Rugby was added to the 1996 games, and  the Triathlon was added in 2016.<br><br>",
+  of overall events has declined. Rugby was added to the 1996 games. The Triathlon was added in 2016.<br><br>",
        x = "",
        y = "",
-       caption = "<b>Data:</b> International Paralympic Committee | <b>Logo:</b> International Paralympic Committee | <b>Design:</b> Jenn Schilling") +
-  theme(axis.text.y = element_text(size = 10),
+       caption = "<b>Data:</b> International Paralympic Committee | <b>Design:</b> Jenn Schilling") +
+  theme(axis.text.y = element_blank(),
+        axis.line.y = element_blank(),
+        axis.ticks.y = element_blank(),
         axis.text.x = element_text(size = 10))
   
   
