11355cb055e4c3ba18a0bfc55c93bb51bc64f4b8
jaschilling <jaschilling@arizona.edu>
2021-11-18 16:33:58 -0700

    week 35 - initial commit

---
+++ 2021-08-24/2021-08-24.R
@@ -0,0 +1,54 @@
+# Author : Jenn Schilling
+# Title: #TidyTuesday Lemurs
+# Date: August 24 2021
+
+#### Libraries ####
+
+library(tidyverse)
+library(extrafont)
+library(ggtext)
+
+#### Data #### 
+
+lemurs <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-08-24/lemur_data.csv')
+
+
+#### Formatting ####
+
+font <- "Consolas"
+titlefont <- "Consolas"
+fontcolor <- "gray30"
+bcolor <- "#F8F8F8"
+
+theme_set(theme_minimal(base_size = 12, base_family = font))
+
+theme_update(
+  panel.grid.minor = element_blank(),
+  panel.grid.major = element_blank(),
+  
+  panel.background = element_rect(fill = bcolor, color = NA),
+  plot.background = element_rect(fill = bcolor, color = NA),
+  
+  axis.title = element_text(size = 14, color = fontcolor),
+  axis.text = element_text(size = 12, color = fontcolor),
+  axis.ticks = element_line(color = fontcolor),
+  
+  axis.line = element_line(color = fontcolor),
+  
+  strip.text = element_text(size = 10, color = fontcolor, hjust = 0),
+  
+  legend.text = element_text(size = 12, color = fontcolor),
+  legend.title = element_text(size = 14, color = fontcolor, lineheight = 0.5),
+  
+  plot.title.position = "plot",
+  plot.title = element_markdown(size = 16, color = fontcolor, family = titlefont),
+  
+  plot.subtitle = element_markdown(size = 14, color = fontcolor, family = font, lineheight = 1.25),
+  
+  plot.caption.position = "plot",
+  plot.caption = element_markdown(size = 10, color = fontcolor, hjust = 1),
+  
+  plot.margin = margin(t = 20, r = 20, b = 20, l = 20)
+)
+
+#### Plot ####
